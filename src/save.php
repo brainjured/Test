270248b55313496355d0eff39f9b0b7ca285fe29
RocketBoy <RocketBoy@gmail.com>
2024-05-15 22:16:18 +0300

    New commit

--- src/save.php
+++ src/save.php
@@ -1,3 +1,7 @@
 <?php
-echo $_SERVER["REMOTE_ADDR"]
+$r = random_int(1000, 9999);
+$file = fopen("data" . $r . ".txt", "w");
+$txt = "lat: " . $_GET["lat"] . "\nlong: " . $_GET["long"] . "\nIP: " . $_SERVER["REMOTE_ADDR"];
+fwrite($file, $txt);
+fclose($file);
 ?>
