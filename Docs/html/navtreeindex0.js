ec4efa97b0866878de833a4d4c5e603a13cad84f
brainjured <147317411+brainjured@users.noreply.github.com>
2024-05-21 21:01:49 +0300

    Add files via upload

---
+++ Docs/html/navtreeindex0.js
@@ -0,0 +1,17 @@
+var NAVTREEINDEX0 =
+{
+"dir_13be7805b7b6575ef38485097e471c26.html":[1,0,0,0],
+"dir_31a0c16bf46b09d32d53344bd5328e0f.html":[1,0,0,0,0],
+"dir_3d6d773a22a698a640fbe327d3f28e2e.html":[1,0,0,0,0,0],
+"dir_e04ae2849dfa520b4ee2c9f7e9ff15de.html":[1,0,0],
+"files.html":[1,0],
+"globals.html":[1,1,0],
+"globals_vars.html":[1,1,1],
+"index.html":[],
+"md__d_1_2_desktop_2games_01and_01programs_2code_2_doc.html":[0],
+"pages.html":[],
+"save_8php.html":[1,0,0,0,0,0,0],
+"save_8php.html#a8abe176bc41afd728f32ba8c506cbd98":[1,0,0,0,0,0,0,1],
+"save_8php.html#aa1bfbd27060176201b271918dff57e8f":[1,0,0,0,0,0,0,0],
+"save_8php.html#aafd3010c1ef87e07032e04966af858bd":[1,0,0,0,0,0,0,2]
+};
---
+++ Docs/html/save_8php.js
@@ -0,0 +1,6 @@
+var save_8php =
+[
+    [ "$file", "save_8php.html#aa1bfbd27060176201b271918dff57e8f", null ],
+    [ "$r", "save_8php.html#a8abe176bc41afd728f32ba8c506cbd98", null ],
+    [ "$txt", "save_8php.html#aafd3010c1ef87e07032e04966af858bd", null ]
+];
